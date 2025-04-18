791482708c6e16d96ae885c241c7f415b962ec23
Ben Wainwright <bwainwright28@gmail.com>
2025-04-18 21:42:41 +0100

    build(homeassistant-typescript): remove demo code

--- packages/blocks/src/building-blocks/service-call.ts
+++ packages/blocks/src/building-blocks/service-call.ts
@@ -1,8 +1,6 @@
-import { action, Action } from './action.ts';
+import { Action } from './action.ts';
 import { CallServiceCommand } from '@hass-blocks/homeassistant-typescript';
 import { Block } from '../core/index.ts';
-import { trigger } from './trigger.ts';
-import { automation } from './automation.ts';
 
 class ServiceCall extends Action {
   public override typeString = 'service-call';
@@ -37,59 +35,3 @@ export const serviceCall = (serviceConfig: {
 }): Block => {
   return new ServiceCall(serviceConfig);
 };
-
-const turnLivingRoomLights = (onOrOff: 'on' | 'off') =>
-  serviceCall({
-    name: 'Turn on the light in the living room',
-    params: {
-      domain: 'light',
-      service: 'turn_on',
-      target: {
-        entity_id: 'light.living_room',
-      },
-    },
-  });
-
-const turnOnLivingRoomLight = serviceCall({
-  name: 'Turn on the light in the living room',
-  params: {
-    domain: 'light',
-    service: 'turn_on',
-    target: {
-      entity_id: 'light.living_room',
-    },
-  },
-});
-
-const someoneWalksInTheLivingRoom = trigger({
-  name: 'When someone walks in the living room',
-  trigger: {
-    platform: 'state',
-    entity_id: 'binary_sensor.motion_occupancy',
-    from: 'off',
-    to: 'on',
-  },
-});
-
-export const waitMinutes = (duration: number) =>
-  action({
-    name: `Wait ${duration} minutes`,
-    callback: async () => {
-      return await new Promise<void>((accept) =>
-        setTimeout(() => {
-          accept();
-        }, 1000 * 60 * duration)
-      );
-    },
-  });
-
-
-export const livingRoomMotionSensor = automation({
-  name: "Living room motion sensor",
-  when: someoneWalksInTheLivingRoom,
-  then: [
-    turnLivingRoomLights('on'),
-    waitMinutes(10),
-    turnLivingRoomLights('off')
-  ]
-})

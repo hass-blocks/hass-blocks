073767942878bb3a408e082b2a38bd3fce0f8555
Ben Wainwright <bwainwright28@gmail.com>
2025-04-18 19:31:36 +0100

    chore(homeassistant-typescript): fixed build errors

--- packages/homeassistant-typescript/src/test-support/get-package-name.ts
+++ packages/homeassistant-typescript/src/test-support/get-package-name.ts
@@ -1,14 +1,10 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
 import { safeJsonParse } from '../lib/utils/index.ts';
-import { IPackageJson } from 'package-jsoVn-type';
+import { IPackageJson } from 'package-json-type';
 
 export const getPackageName = () => {
   const projectRoot = join(import.meta.dirname, '..', '..');
   const contents = readFileSync(join(projectRoot, './package.json'), 'utf8');
   return safeJsonParse<IPackageJson>(contents)['name'];
 };
-
-class PackageJson {
-  public constructor()
-}

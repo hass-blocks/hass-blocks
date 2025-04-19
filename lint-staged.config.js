311c58c60b21cf584d05bdc6144233c0e9825fd7
Ben Wainwright <bwainwright28@gmail.com>
2025-04-19 10:50:33 +0100

    chore(root): format code and add prettier and lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,7 +3,7 @@
  * @type {import('lint-staged').Configuration}
  */
 const config = {
-  '*.{js,jsx,ts,tsx,json,css,md,html}': ['prettier --write', 'git add'],
+  '*.{js,jsx,ts,tsx,json,css,md,html}': ['prettier --write'],
 };
 
 module.exports = config;

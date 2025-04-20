854e2025048c28be2fff688fefcab2429a67c1f7
Ben Wainwright <bwainwright28@gmail.com>
2025-04-20 11:35:27 +0100

    chore(ci): fixed lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,24 @@
+const path = require('path');
+
 /**
  * @filename: lint-staged.config.js
  * @type {import('lint-staged').Configuration}
  */
 const config = {
   '*.{js,jsx,ts,tsx,json,css,md,html,mts,mjs,cts,cjs}': ['prettier --write'],
-  '*.{jsx,ts,tsx}': ['vitest related'],
-  '*.{jsx,ts,tsx}': ['tsc-files --noEmit'],
+  '*.{js,jsx,ts,tsx,mts,mjs,cts,cjs}': [
+    'vitest related',
+    (paths) => {
+      try {
+        const cwd = process.cwd();
+        const relativePaths = paths.map((file) => path.relative(cwd, file));
+        console.log('end');
+        return `npx nx affected --targets rollup-api --files ${relativePaths.join(' ')}`;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  ],
 };
 
 module.exports = config;

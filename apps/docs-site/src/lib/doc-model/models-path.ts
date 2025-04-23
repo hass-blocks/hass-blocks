460ac1f44213184496b2934128ba5ef2d88b8d9b
Ben Wainwright <bwainwright28@gmail.com>
2025-04-23 07:56:12 +0100

    chore(ci): fix cdk

--- apps/docs-site/src/lib/doc-model/models-path.ts
+++ apps/docs-site/src/lib/doc-model/models-path.ts
@@ -1,3 +1,4 @@
 import { join } from 'path';
+import { cwd } from 'process';
 
-export const modelsPath = join(import.meta.dirname, '..', '..', 'models');
+export const modelsPath = join(cwd(), 'apps', 'docs-site', 'models');

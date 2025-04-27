d94bce2505c2b7558929c8fa45a2e817a437e0be
Ben Wainwright <bwainwright28@gmail.com>
2025-04-27 22:58:07 +0100

    fix(line-plugin): tweaked layout

--- packages/simple-terminal-logging-plugin/src/lib/components/execution-line/execution-line.tsx
+++ packages/simple-terminal-logging-plugin/src/lib/components/execution-line/execution-line.tsx
@@ -27,14 +27,13 @@ export const ExecutionLine = ({ event }: ExecutionLineProps) => {
   return (
     <Box flexDirection="row" width="100%">
       <Box width={4}>
-        <Text>{getIcon(event)}</Text>
+        <Text> {getIcon(event)}</Text>
       </Box>
-      <Box width={12}>
-        <Text>{event.eventType}</Text>
-      </Box>
-
       <Box width={35}>
         <Text>{'parent' in event && event.parent?.name}</Text>
+      </Box>
+      <Box width={30}>
+        <Text>{event.eventType}</Text>
       </Box>
       <Box width={40}>
         <Text>{event.name}</Text>

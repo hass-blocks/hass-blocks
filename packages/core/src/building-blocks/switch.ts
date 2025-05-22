d647889213dbe838014f86e38567217b5eb23be2
Ben Wainwright <bwainwright28@gmail.com>
2025-05-22 11:55:04 +0100

    chore(core): remove dead code

--- packages/core/src/building-blocks/switch.ts
+++
@@ -1,4 +0,0 @@
-import type { ISwitchable } from '@types';
-
-export const turn = (switchable: ISwitchable, onOrOff: 'on' | 'off') =>
-  onOrOff === 'on' ? switchable.switchOn() : switchable.switchOff();

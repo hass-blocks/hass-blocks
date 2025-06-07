04ecbd67228f20204afcf739713046030efd7778
Ben Wainwright <bwainwright28@gmail.com>
2025-06-07 09:22:48 +0100

    fix(cli): --watch mode was accidentilly switching on for everything

--- packages/cli/src/commands/gen-code.ts
+++ packages/cli/src/commands/gen-code.ts
@@ -28,25 +28,25 @@ export class GenCodeCommand extends Command {
 
       if (this.watch) {
         console.log('Watching Home Assistant for changes');
+
+        const regenerateEvents = [
+          'homeassistant_start',
+          'service_removed',
+          'service_registered',
+          'automation_reloaded',
+          'component_loaded',
+          'core_config_updated',
+        ];
+
+        client.subscribeToEvents(async (event) => {
+          if (
+            'event_type' in event &&
+            regenerateEvents.includes(event.event_type)
+          ) {
+            await doCodegen(client, this.folder);
+          }
+        });
       }
-
-      const regenerateEvents = [
-        'homeassistant_start',
-        'service_removed',
-        'service_registered',
-        'automation_reloaded',
-        'component_loaded',
-        'core_config_updated',
-      ];
-
-      client.subscribeToEvents(async (event) => {
-        if (
-          'event_type' in event &&
-          regenerateEvents.includes(event.event_type)
-        ) {
-          await doCodegen(client, this.folder);
-        }
-      });
     } finally {
       await client.close();
     }

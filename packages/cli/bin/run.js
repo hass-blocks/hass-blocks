8bf6a510308860b4589b96c00da4273d7e7df78d
Ben Wainwright <bwainwright28@gmail.com>
2025-06-07 01:57:03 +0100

    fix(cli): correct entry point file to point to the right path

--- packages/cli/bin/run.js
+++ packages/cli/bin/run.js
@@ -3,5 +3,5 @@
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 const currentDirectory = dirname(fileURLToPath(import.meta.url));
-const runFile = join(currentDirectory, '..', 'dist', 'src', 'run.js');
+const runFile = join(currentDirectory, '..', 'dist', 'run.js');
 await import(runFile);

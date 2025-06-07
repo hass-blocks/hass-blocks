ea159d50cfc3b2dd29a8effcd792ab4355609199
Ben Wainwright <bwainwright28@gmail.com>
2025-06-07 21:21:25 +0100

    chore(root): stuff

--- hass-blocks-addon/run.sh
+++ hass-blocks-addon/run.sh
@@ -1,13 +1,10 @@
 BLOCKS_DIR=/config/hass-blocks
 BIN_DIR=/addon/node_modules/.bin
 
-mkdir -p $BLOCKS_DIR || true
-
-$BIN_DIR/has-blocks load \
+$BIN_DIR/hass-blocks load \
     --folder $BLOCKS_DIR \
-    --codegenOutput $BLOCKS_DIR/.blocks & \
-    --websocketHost 0.0.0.0
-    --websocketPort 8080
+    --websocketHost 0.0.0.0 \
+    --websocketPort 8080 & \
 
 $BIN_DIR/vite-node \
     /src/node/serve-frontend.ts \

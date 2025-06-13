325f35711f1a9d9d31c813b2aca7579648eeb6b6
Ben Wainwright <bwainwright28@gmail.com>
2025-06-13 19:06:22 +0100

    feat(cli): now aware of its version

--- packages/cli/src/run.ts
+++ packages/cli/src/run.ts
@@ -1,4 +1,5 @@
 import { genCode, load, create } from '@commands';
+import packageJson from '../package.json' with { type: 'json' };
 import { run } from '@drizzle-team/brocli';
 
-run([load, genCode, create]);
+run([load, genCode, create], { version: packageJson.version });
